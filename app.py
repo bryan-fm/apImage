5eeb9331c8e91f35f9c171a2c016806777e80136
bryan-fm <bryanfranca2@hotmail.com>
2020-06-26 11:58:38 -0300

    alterando index

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    response["MESSAGE"] = f"Ticaracatica"
+    return jsonify(response)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
