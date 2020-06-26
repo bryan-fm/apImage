6e91d76963c8babb9e13e9f6de6d7364c72b7cf2
bryan-fm <bryanfranca2@hotmail.com>
2020-06-26 12:00:46 -0300

    corrigindo index

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
+    response = {}
     response["MESSAGE"] = f"Ticaracatica"
     return jsonify(response)
 
